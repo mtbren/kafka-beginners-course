79914b8bf535c4d9dbf8d44edaf43a869a0d484a
mtbren <bh.amit82@gmail.com>
2023-02-14 19:50:05 -0500

    Initial commit

---
+++ kafka-basics/src/main/java/org/internal/priv8/demos/kafka/ProducerDemo.java
@@ -0,0 +1,40 @@
+package org.internal.priv8.demos.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemo {
+    private static final Logger log = LoggerFactory.getLogger(ProducerDemo.class.getSimpleName());
+    public static void main(String[] args) {
+        log.info("This works!!!");
+
+        // Create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers","localhost:9092");
+        properties.setProperty("key.serializer", StringSerializer.class.getName());
+        properties.setProperty("value.serializer", StringSerializer.class.getName());
+
+
+
+        // Create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        // Create a Producer Record
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>("demo_java","Check Check Check");
+
+        // Send the message/data to the Producer
+        producer.send(producerRecord);
+
+        // Flush the Producer
+        // Tells the producer to flush all data and block until its done --> synchronous
+        producer.flush();
+
+        // Flush and Close the Producer
+        producer.close();
+    }
+}
